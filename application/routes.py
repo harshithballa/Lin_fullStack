016273201c9f5d5d8314d9b6fcdbedf70588dff5
sai harshith balla <saiharshithballa99@gmail.com>
2020-08-23 16:59:21 +0530

    Inserting data

--- application/routes.py
+++ application/routes.py
@@ -46,7 +46,8 @@ def api(idx = None):
 
 
 class User(db.Document):
-    user_id = db.IntField( unique=True )
+    _id = db.StringField()
+    id = db.IntField( unique=True )
     first_name = db.StringField( max_length=50 )
     last_name = db.StringField( max_length=50 )
     email = db.StringField(max_length=30)
